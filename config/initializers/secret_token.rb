8338bb36ff06a2815a7a4c4315944150c9ee2a76
JulesWiz <ngjulie0203@gmail.com>
2014-06-17 03:21:08 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WGS1::Application.config.secret_key_base = 'a644df5ac488f6967ed9b9e0c4797bed8f2c63f8369bebdcc2def027726a19787ce06b0d05fe8243709c27c18de0da997a35737e04287de2c03ec5cb769e7198'
